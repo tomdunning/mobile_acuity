ddb7d439c7c55850f1e08fc0bd08eb075c9c9e40
Tom Dunning <twdunning@gmail.com>
2015-06-24 15:09:49 +0100

    text/json should have been application/json

--- mobile_acuity.rb
+++ mobile_acuity.rb
@@ -7,7 +7,7 @@ module MobileAcuity
       curl = Curl::Easy.new("http://api.mobileacuity.net/v1/data/#{DATANAME}/datasets/#{dataset}/images?value=#{URI.encode(some_value)}")
       # MA will give you a "400 - Bad Request" if you try removing the params hash, there needs to be at least 1 kv pair
       curl.headers["Content-Type"] = "image/jpeg"
-      curl.headers["Accept"] = "text/json"
+      curl.headers["Accept"] = "application/json"
       curl.post_body = File.read(image_path)
       curl.http_post
       
@@ -19,7 +19,7 @@ module MobileAcuity
       curl = Curl::Easy.new("http://api.mobileacuity.net/v1/search/#{DATANAME}/#{dataset}")
       curl.headers["Content-Type"] = "image/jpeg"
       if accept == :json
-        curl.headers["Accept"] = "text/json"
+        curl.headers["Accept"] = "application/json"
       end
       curl.post_body = File.read(image_path)
       curl.http_post
